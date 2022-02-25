e0f0b2a60b89abb969b2a7913ff3ae1be8af2f58
Kr36pton <jmmesa37@misena.edu.co>
2022-02-25 00:04:00 -0500

    Se crea la funcion de generar listas de deberes

--- front/src/components/StoreProvider.jsx
+++ front/src/components/StoreProvider.jsx
@@ -1,55 +1,67 @@
-import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
-
+import React, { useReducer, createContext } from 'react';
 const initialState = {
-  todo: { list: [], item: {} }
+  todo: { 
+      groupToDo:[], 
+      item: {} 
+  }
 };
 export const Store = createContext(initialState);
-
-const StoreProvider = ({ children }) => {
-
-      function reducer(state, action) {
-        switch (action.type) {
-          case 'update-item':
-            const todoUpItem = state.todo;
-            const listUpdateEdit = todoUpItem.list.map((item) => {
-              if (item.id === action.item.id) {
-                return action.item;
-              }
-              return item;
-            });
-            todoUpItem.list = listUpdateEdit;
-            todoUpItem.item = {};
-            return { ...state, todo: todoUpItem }
-          case 'delete-item':
-            const todoUpDelete = state.todo;
-            const listUpdate = todoUpDelete.list.filter((item) => {
-              return item.id !== action.id;
-            });
-            todoUpDelete.list = listUpdate;
-            return { ...state, todo: todoUpDelete }
-          case 'update-list':
-            const todoUpList = state.todo;
-            todoUpList.list = action.list;
-            return { ...state, todo: todoUpList }
-          case 'edit-item':
-            const todoUpEdit = state.todo;
-            todoUpEdit.item = action.item;
-            return { ...state, todo: todoUpEdit }
-          case 'add-item':
-            const todoUp = state.todo.list;
-            todoUp.push(action.item);
-            return { ...state, todo: {list: todoUp, item: {}} }
-          default:
-            return state;
+function reducer(state, action) {
+  switch (action.type) {
+    case 'update-item':
+      const todoUpItem = state.todo;
+      const listUpdateEdit = todoUpItem.list.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item;
         }
-      }
+        return item;
+      });
+      todoUpItem.list = listUpdateEdit;
+      todoUpItem.item = {};
+      return { ...state, todo: todoUpItem }
+    case 'delete-item':
+      const todoUpDelete = state.todo;
+      const listUpdate = todoUpDelete.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      todoUpDelete.list = listUpdate;
+      return { ...state, todo: todoUpDelete }
+    case 'update-list':
+      const listGroup = action.groupToDo;
+      state.todo.groupToDo = action.groupToDo;
+      return {todo: {groupToDo: listGroup}}
+    case 'edit-item':
+      const todoUpEdit = state.todo;
+      todoUpEdit.item = action.item;
+      return { ...state, todo: todoUpEdit }
+    case 'add-item-group':
+      const todoGroupUp = state.todo.groupToDo;
+      todoGroupUp.push({...action.groupToDo, tasks:[]});
+      state.todo.groupToDo = todoGroupUp;
+      return {todo: {groupToDo: todoGroupUp}};
+    case 'add-item':
+      const todoUp = state.todo.groupToDo.map(
+        (group) =>
+        { 
+          if (group.id == action.groupId)
+              group.tasks.push(action.item);
 
+          return group;
+        });
+
+      state.todo.groupToDo = todoUp;
+      console.log(todoUp);
+      return {todo: {groupToDo: todoUp} }
+    default:
+      return state;
+  }
+}
+const StoreProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-
+  
     return <Store.Provider value={{ state, dispatch }}>
       {children}
     </Store.Provider>
-
-  }
-
+  
+}
 export default StoreProvider;
--- front/src/components/TableItems.jsx
+++ front/src/components/TableItems.jsx
@@ -1,76 +1,146 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Store } from "./StoreProvider";
 
 const TableItems = () => {
-    const HOST_API = "http://localhost:8080/api";
+    
+  const HOST_API = "http://localhost:8080/api/to-do";
 
-    const { dispatch, state: { todo } } = useContext(Store);
-    const currentList = todo.list;
+  const { register, errors, handleSubmit } = useForm();
 
-    useEffect(() => {
-      fetch(HOST_API + "/todos")
-        .then(response => response.json())
-        .then((list) => {
-          dispatch({ type: "update-list", list })
-        })
-    });
+  const { dispatch, state: { todo } } = useContext(Store);
+  //const currentList = todo.list;
+  const [groups, setGroups] = useState([]);
+  const currentList = [{}];
+  useEffect(() => {
+    fetch(HOST_API + "/groups/list")
+      .then(response => response.json())
+      .then((groups) => {
+        dispatch({ type: "update-list", groupToDo: groups })
+        setGroups(todo.groupToDo);
+      });
+
+  }, [dispatch]);
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE"
+    }).then((list) => {
+      dispatch({ type: "delete-item", id })
+    })
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo })
+  };
+
+  const onChange = (event, todo) => {
+    const request = {
+      name: todo.name,
+      id: todo.id,
+      completed: event.target.checked
+    };
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+      });
+};
 
 
-    const onDelete = (id) => {
-      fetch(HOST_API + "/" + id + "/todo", {
-        method: "DELETE"
-      }).then((list) => {
-        dispatch({ type: "delete-item", id })
-      })
+  const decorationDone = {
+    textDecoration: 'line-through'
+  };
+
+  const addTaskToGroup = (data) => {
+    const request = {
+      name: data.name,
+      completed: false,
+      tasks:[]
     };
 
-    const onEdit = (todo) => {
-      dispatch({ type: "edit-item", item: todo })
-    };
+    fetch(HOST_API + "/items/save/" + data.groupId, {
+      method: "POST",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "add-item", item: todo, groupId: data.groupId });
+      });
+}
 
-    const onChange = (event, todo) => {
-      const request = {
-        name: todo.name,
-        id: todo.id,
-        completed: event.target.checked
-      };
-      fetch(HOST_API + "/todo", {
-        method: "PUT",
-        body: JSON.stringify(request),
-        headers: {
-          'Content-Type': 'application/json'
+  const mark = () => {
+  }
+
+  return <div>
+    <form onSubmit={handleSubmit(addTaskToGroup)}>
+      <input {...register("name")} type="text" placeholder="Nombre del To-Do" />
+      <select {...register("groupId")}>
+        <option>Seleccione una categoria</option>
+        {
+          groups.map((group) => {
+            return (
+              <option value={group.id}>{group.name}</option>
+            );
+          })
         }
-      })
-        .then(response => response.json())
-        .then((todo) => {
-          dispatch({ type: "update-item", item: todo });
-        });
-    };
+      </select>
+      <button type="submit">Crear tarea</button>
+    </form>
 
-    const decorationDone = {
-      textDecoration: 'line-through'
-    };
-    return <div>
-      <table >
+    {
+      groups.map((group) => {
+        return (
+          <div>
+            <h3>{group.name}</h3>
+            <form onChange={handleSubmit(mark)}>
+              <table>
+                <thead>
+                  <tr>
+                    <td>ID</td>
+                    <td>Tarea</td>
+                    <td>¿Completado?</td>
+                  </tr>
+                </thead>
+                <tbody>
+                {
+                  group.tasks!=null?group.tasks.map((task) => {
+                    return (
+                      <tr>
+                        <td>{task.id}</td>
+                        <td>{task.name}</td>
+                        <td><input type="checkbox" />Completado</td>
+                      </tr>
+                    );
+                  }): <h3>No tiene actividades en esta seccion</h3>
+                }
+                </tbody>
+              </table>
+
+            </form>
+
+          </div>
+        );
+      })
+    }
+    {/* <table >
         <thead>
           <tr>
-            <td>Id</td>
-            <td>Deber</td>
-            <td>¿Finalizado?</td>
-          </tr>
-        </thead>
-        <tbody>
-          {currentList.map((todo) => {
-            return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
-              <td>{todo.id}</td>
-              <td>{todo.name}</td>
-              <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
-              <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
-              <td><button onClick={() => onEdit(todo)}>Editar</button></td>
+            <td>ID</td>
+	@@ -87,7 +156,7 @@ const TableItems = () => {
             </tr>
           })}
         </tbody>
-      </table>
-    </div>
+      </table> */}
+  </div>
 }
 export default TableItems;
