d6d8f086b72761b38ba641dd4fa0d72911ee739b
Kr36pton <jmmesa37@misena.edu.co>
2022-02-24 23:22:01 -0500

    Implementacion mappers en controlador

--- back/src/main/java/co/com/sofka/crud/controllers/ToDoController.java
+++ back/src/main/java/co/com/sofka/crud/controllers/ToDoController.java
@@ -2,11 +2,10 @@ package co.com.sofka.crud.controllers;
 import co.com.sofka.crud.entities.ItemDTO;
 import co.com.sofka.crud.entities.ToDo;
 import co.com.sofka.crud.services.InterfaceToDoService;
-import co.com.sofka.crud.services.ToDoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
 @RequestMapping("/api/to-do/items")
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -15,11 +14,12 @@ public class ToDoController {
     private InterfaceToDoService service;
     @Autowired
     private ModelMapper mapper;
-    @PostMapping(value = "/save/{listGroup}")
-    public ItemDTO save(@RequestBody ItemDTO itemDTO,@PathVariable Long listGroup)
+
+    @PostMapping(value = "/save/{listGroupId}")
+    public ItemDTO save(@RequestBody ItemDTO itemDTO,@PathVariable Long listGroupId)
     {
         ToDo toDo = mapper.map(itemDTO, ToDo.class);
-        return service.save(toDo, listGroup);
+        return service.save(toDo, listGroupId);
     }
 
     @GetMapping(value = "/list")
@@ -30,13 +30,11 @@ public class ToDoController {
     public ItemDTO get(@PathVariable("id") Long id){
         return service.get(id);
     }
-
     @PutMapping(value = "/update")
     public ItemDTO update(@RequestBody ItemDTO itemDTO){
         ToDo toDo = mapper.map(itemDTO, ToDo.class);
         return service.update(toDo);
     }
-
     @DeleteMapping(value = "/delete/{id}")
     public void delete(@PathVariable("id")Long id){
         service.delete(id);
