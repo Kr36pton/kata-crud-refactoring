1dced081d291495f20429fc1c90c6740183ac55f
Kr36pton <jmmesa37@misena.edu.co>
2022-02-25 12:45:17 -0500

    Se crea la funcion eliminar

--- front/src/components/TableItems.jsx
+++ front/src/components/TableItems.jsx
@@ -3,13 +3,10 @@ import { useForm } from "react-hook-form";
 import { Store } from "./StoreProvider";
 const TableItems = () => {
   const HOST_API = "http://localhost:8080/api/to-do";
-
   const { register, errors, handleSubmit, setValue } = useForm();
-
   const { dispatch, state: { todo } } = useContext(Store);
   const [currentList, setCurrentList] =  useState({name: ""});
   const [groups, setGroups] = useState([]);
-
   useEffect(() => {
     fetch(HOST_API + "/groups/list")
       .then(response => response.json())
@@ -17,11 +14,8 @@ const TableItems = () => {
         dispatch({ type: "update-list", groupToDo: groups })
         setGroups(todo.groupToDo);
       });
-
   }, [dispatch]);
-
   setValue("nameItemUpdate",currentList.nameItemUpdate);
-
   const onDelete = (id) => {
     fetch(HOST_API + "/" + id + "/todo", {
       method: "DELETE"
@@ -29,10 +23,8 @@ const TableItems = () => {
       dispatch({ type: "delete-item", id })
     })
   };
-
   const onEdit = (todo) => {
   };
-
   const onChange = (event, todo) => {
     const request = {
       name: todo.name,
@@ -54,7 +46,6 @@ const TableItems = () => {
   const decorationDone = {
     textDecoration: 'line-through'
   };
-
   const addTaskToGroup = (data) => {
     const request = {
       name: data.nameItem,
@@ -75,9 +66,7 @@ const TableItems = () => {
       });
   }
   const mark = () => {
-
   }
-
   const onUpdate = (data) => 
   {
     const request = {
@@ -98,9 +87,7 @@ const TableItems = () => {
         dispatch({type: "edit-item", task: data, groupId: currentList.idItemUpdate});
     })
   }
-
   return <div>
-
     <form onSubmit={handleSubmit(addTaskToGroup)}>
       <input {...register("nameItem")} type="text" placeholder="Nombre del To-Do" />
       <select {...register("groupId")}>
@@ -115,14 +102,10 @@ const TableItems = () => {
       </select>
       <button type="submit">Crear tarea</button>
     </form>
-
-
-
     <form onSubmit={handleSubmit(onUpdate)}>
         <input type="text" {...register("nameItemUpdate")}/>
         <button type="submit">Actualizar</button>
     </form>
-
     {
       groups.map((group) => {
         return (
@@ -133,10 +116,10 @@ const TableItems = () => {
                 <thead>
                   <tr>
                     <th>ID</th>
-                    <th>Chores</th>
-                    <th>Completed?</th>
-                    <th>Edit</th>
-                    <th>Delete</th>
+                    <th>Tarea</th>
+                    <th>¿Completado?</th>
+                    <th>Editar</th>
+                    <th>Eliminar</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -146,17 +129,26 @@ const TableItems = () => {
                       <tr>
                         <td>{task.id}</td>
                         <td>{task.name}</td>
-                        <td><input type="checkbox" />Completed</td>
+                        <td><input type="checkbox" />Completado</td>
                         <td><button type="submit" onClick={(event) =>
                           {
                             event.preventDefault();
                             setCurrentList({"nameItemUpdate": task.name, "idItemUpdate": task.id});
 
-                          }}>Edit</button></td>
-                        <td><button type="submit">Delete</button></td>
+                          }}>Editar</button></td>
+                        <td><button type="submit" onClick={(event)=>
+                          {
+                            event.preventDefault();
+                            fetch(HOST_API + "/items/delete/" + task.id, {
+                              method: "DELETE",
+                              headers: {
+                                'Content-Type': 'application/json'
+                              }
+                            })
+                          }}>Eliminar</button></td>
                       </tr>
                     );
-                  }): <h3>You dont have any chores on this section</h3>
+                  }): <h3>No tiene actividades en esta seccion</h3>
                 }
                 </tbody>
               </table>
@@ -165,26 +157,6 @@ const TableItems = () => {
         );
       })
     }
-    {/* <table >
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>Tarea</td>
-            <td>¿Completado?</td>
-          </tr>
-        </thead>
-        <tbody>
-          {currentList.map((todo) => {
-            return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
-              <td>{todo.id}</td>
-              <td>{todo.name}</td>
-              <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
-              <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
-              <td><button onClick={() => onEdit(todo)}>Editar</button></td>
-            </tr>
-          })}
-        </tbody>
-      </table> */}
   </div>
 }
 export default TableItems;
