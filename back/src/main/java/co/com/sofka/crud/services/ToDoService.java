0bee77b3735c7f544623c79993503a05ae04a85a
Kr36pton <jmmesa37@misena.edu.co>
2022-02-24 19:37:28 -0500

    Se crean los servicios ToDo

---
+++ back/src/main/java/co/com/sofka/crud/services/ToDoService.java
@@ -0,0 +1,29 @@
+package co.com.sofka.crud.services;
+
+import co.com.sofka.crud.repositories.ToDoRepository;
+import co.com.sofka.crud.entities.ToDo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ToDoService {
+
+    @Autowired
+    private ToDoRepository repository;
+
+    public Iterable<ToDo> list(){
+        return repository.findAll();
+    }
+
+    public ToDo save(ToDo todo){
+        return repository.save(todo);
+    }
+
+    public void delete(Long id){
+        repository.delete(get(id));
+    }
+
+    public ToDo get(Long id){
+         return repository.findById(id).orElseThrow();
+    }
+}
